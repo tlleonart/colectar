c98d3140bec8d468685e81b628ac9c5ccdd86eb7
Tomás <tomasplleonart@gmail.com>
2025-05-30 22:46:20 +0200

    fix

--- src/modules/shared/components/modals/modal-wrapper.tsx
+++ src/modules/shared/components/modals/modal-wrapper.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import { DonateModal } from "./donate-modal";
 import { VolunteerModal } from "./volunteer-modal";
 
 export const ModalWrapper: FC = () => {
   return (
-    <>
+    <Suspense>
       <DonateModal />
       <VolunteerModal />
-    </>
+    </Suspense>
   );
 };
